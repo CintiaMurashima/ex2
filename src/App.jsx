b40c01d54db74d951ba90c402301c8ec8f74c2c8
CintiaMurashima <cintiamurashima@gmail.com>
2023-03-21 17:34:11 +0100

    ultimo

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,48 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function App() {
 
-const [item, setItem]= useState("");
-const [list, setList]= useState([]);
+  const [item, setItem] = useState("");
+  const [lista, setLista] = useState(['aaa', 'bbb', 'ccc']);
+  const listaItems = lista.map((el, index) => <li key={index}>{el}</li>);
+  const listaFilas = lista.map((el, index) => <tr key={index}><td>{index}</td><td>{el}</td></tr>);
+  function nuevoItem() {
+    const nuevaList = [...lista];
+    nuevaList.push(item);
+    setLista(nuevaList);
+    setItem("");
+  }
+
 
   return (
 
     <div className="App">
       <container>
         <h1>Hola React</h1>
-
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
-    </Form>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Entra valor</Form.Label>
+            <Form.Control type="text" value={item} onInput={(e) => setItem(e.target.value)} placeholder="Enter email" />
+          </Form.Group>
+          <Button variant="success" onClick={nuevoItem}>Añadir</Button>
+        </Form>
+        <hr />
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Item</th>
+            </tr>
+          </thead>
+          <tbody>
+            {listaFilas}
+          </tbody>
+        </Table>
 
       </container>
     </div>
