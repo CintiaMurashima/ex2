3e6b6ff17fa0885ef48eab670524ea968a4a2374
CintiaMurashima <cintiamurashima@gmail.com>
2023-03-21 16:32:42 +0100

    second

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,21 @@ import Form from 'react-bootstrap/Form';
 
 function App() {
 
+const [item, setItem]= useState("");
+const [list, setList]= useState([]);
 
   return (
+
     <div className="App">
       <container>
         <h1>Hola React</h1>
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+    </Form>
 
       </container>
     </div>
